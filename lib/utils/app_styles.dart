b25d8e7e99acbe18d33770a2d6897ed0fbd6c94a
arpit <arpitsahu789@gmail.com>
2023-03-21 16:54:50 +0530

    created app style and size config

---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+
+const kBlackColor  = Color(0xff000000);
+const kGeryColor = Color(0xff464646);
+
+final kQuestrialBold = GoogleFonts.questrial(
+  fontWeight: FontWeight.w700,
+);
+
+final kQuestrialSemiBold = GoogleFonts.questrial(
+  fontWeight: FontWeight.w600,
+);
+
+final kQuestrialMedium = GoogleFonts.questrial(
+  fontWeight: FontWeight.w500,
+);
+
+final kQuestrialRegular = GoogleFonts.questrial(
+  fontWeight: FontWeight.w400,
+);
