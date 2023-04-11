655128c51d2ebc55468eafb1661b6668796c5772
arpit <arpitsahu789@gmail.com>
2023-04-11 07:15:18 +0530

    use pageview to showcase designs

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,49 @@ import 'package:ui_pracitice/screens/splash_screen.dart';
 import 'package:ui_pracitice/screens/storage_detail_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+
+
+   MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Cloud Storage App',
-      home: StorageDetailScreen(),
+      home: Scaffold(
+        body: PageView(
+          controller: _pageController,
+          children: [
+            SplashScreen(),
+            HomeScreen(),
+            StorageDetailScreen(),
+          ],
+        ),
+      ),
     );
   }
 }
